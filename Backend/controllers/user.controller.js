55cb73f1aca055455359f262ba5377cc4d4ac65d
Jatin Gupta <jatin04gupta2004@gmail.com>
2025-04-20 15:31:58 +0530

    updated samesite issue

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
             phoneNumber: user.phoneNumber,
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None' }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
@@ -80,61 +80,6 @@ export const login = async (req, res) => {
         console.log(error);
     }
 }
-
-// export const updateProfile = async (req, res) => {
-//     try {
-//         const { fullname, email, phoneNumber, bio, skills } = req.body;
-//         const file = req.file;
-//         // cloudinary aayega idhar
-//         const fileUri = getDataUri(file)
-//         const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-
-//         let skillsArray;
-//         if (skills) {
-//             skillsArray = skills.split(",");
-//         }
-//         const userId = req.id;  // will come from middleware authentication
-//         let user = await User.findById(userId);
-
-//         if (!user) {
-//             return res.status(400).json({
-//                 message: "User not found",
-//                 sucsess: false
-//             })
-//         };
-
-//         // updating data
-//         if (fullname) user.fullname = fullname
-//         if (email) user.email = email
-//         if (phoneNumber) user.phoneNumber = phoneNumber
-//         if (bio) user.profile.bio = bio
-//         if (skills) user.profile.skills = skillsArray
-//         // resume comes later here
-//         if (cloudResponse) {
-//             user.profile.resume = cloudResponse.secure_url
-//             user.profile.resumeOriginalName = file.originalname
-//         }
-//         await user.save();
-
-//         user = {
-//             _id: user._id,
-//             fullname: user.fullname,
-//             email: user.email,
-//             phoneNumber: user.phoneNumber,
-//             role: user.role,
-//             profile: user.profile,
-//         }
-
-//         return res.status(200).json({
-//             message: "Profile updated successfully",
-//             user,
-//             success: true
-//         })
-
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
 
 export const logout = async (req, res) => {
     try {
